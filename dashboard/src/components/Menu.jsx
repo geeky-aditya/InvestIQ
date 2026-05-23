0a960112b635b06f17e62c700a226361475f834e
geeky-aditya <adi.kr20004@gmail.com>
2026-05-23 05:56:23 +0530

    logout

--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -11,7 +11,7 @@ const Menu = () => {
   }
   const handleLogout = () => {
   localStorage.removeItem("token");
-  window.location.href = "https://investiq-dashboard.onrender.com/login";
+  window.location.href = "https://investiq-frontend-xc5x.onrender.com/login";
 };
   const menuClass = "menu";
   const activeMenuClass = "menu selected";
